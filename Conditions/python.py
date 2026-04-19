5f758793e6632415faef2f082b12de80e25308e8
Ayesha Nazar <ayesha.nazar@plcgroup.com>
2026-04-19 14:40:25 +0500

    snack suggestion program

---
+++ Conditions/python.py
@@ -0,0 +1,7 @@
+snack = input("Enter your preferred Snack:").lower()
+
+if snack == "samosa" or snack == "cookie":
+    print("Order is Confirmed!")
+
+else:
+    print("Sorry! We don't have that snack.")
