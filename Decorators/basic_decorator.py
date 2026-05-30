0d1a2c0f69acfaee1b1bbd92b7fa1498949fb373
Ayesha123413 <Ayeshanazar21619@gmail.com>
2026-05-30 16:11:43 +0500

    minor chnages

--- Decorators/basic_decorator.py
+++ Decorators/basic_decorator.py
@@ -1,5 +1,8 @@
+from functools import wraps
+
 def my_decor(func):
     # print("Inside the decorator function.", func)
+    @wraps(func)
     def wrapper():
         print("Before the function call.")
         func()
@@ -7,10 +10,13 @@ def my_decor(func):
     return wrapper
 
 
-# @my_decor
+@my_decor
 #whatever comes after @my_decor will be passed as an argument to my_decor function
 def say_hello():
     print("Hello, World!")
 
 
 say_hello()
+
+# without usinf functools warsps function name will be wrapper instead of say_hello because wrapper is the function that is returned by my_decor function and it is the function that is called when we call say_hello function. So to preserve the original function name we can use functools wraps decorator. It will preserve the original function name and other attributes of the original function.
+print(say_hello.__name__)
